ce70af3d8d154f0e476ae61a82709a9d05c4ef3f
rlotr02 <0314kelee@naver.com>
2024-10-26 10:55:37 +0900

    feat: 결과 이미지 추가

--- src/constants/ImageData.tsx
+++ src/constants/ImageData.tsx
@@ -1,35 +1,117 @@
 import Image1 from "@images/cards/1.png";
 import Image2 from "@images/cards/2.png";
+import Image3 from "@images/cards/3.png";
+import Image4 from "@images/cards/4.png";
+import Image5 from "@images/cards/5.png";
+import Image6 from "@images/cards/6.png";
+import Image7 from "@images/cards/7.png";
+import Image8 from "@images/cards/8.png";
+import Image9 from "@images/cards/9.png";
+import Image10 from "@images/cards/10.png";
+import Image11 from "@images/cards/11.png";
+import Image12 from "@images/cards/12.png";
+import Image13 from "@images/cards/13.png";
+import Image14 from "@images/cards/14.png";
+import Image15 from "@images/cards/15.png";
+import Image16 from "@images/cards/16.png";
+import Image17 from "@images/cards/17.png";
+import Image18 from "@images/cards/18.png";
+import Image19 from "@images/cards/19.png";
 
 export const ImageData = [
   {
-    name: "행운두기",
+    name: "키 커지는 부적",
     image: Image1,
-    color: "#ff0000",
+    color: "#C2DBE0",
   },
   {
-    name: "럭키두기",
+    name: "행운 만땅 부적",
     image: Image2,
-    color: "#ff9100",
+    color: "#BAE1BC",
   },
   {
-    name: "히히두기",
-    image: Image1,
-    color: "#00ff00",
+    name: "돈을 줍는 부적",
+    image: Image3,
+    color: "#E9D0A1",
   },
   {
-    name: "행두기",
-    image: Image2,
-    color: "#0095ff",
+    name: "부자가 되는 부적",
+    image: Image4,
+    color: "#B2D4ED",
   },
   {
-    name: "운두기",
-    image: Image1,
-    color: "#f40aae",
+    name: "행복해지는 부적",
+    image: Image5,
+    color: "#D3BBB0",
   },
   {
-    name: "운행두기",
-    image: Image2,
-    color: "#b300ff",
+    name: "웃음이 많아지는 부적",
+    image: Image6,
+    color: "#FFE684",
+  },
+  {
+    name: "합격 부적",
+    image: Image7,
+    color: "#FBC4C4",
+  },
+  {
+    name: "사랑이 이뤄지는 부적",
+    image: Image8,
+    color: "#FDE0FF",
+  },
+  {
+    name: "성적 잘 받는 부적",
+    image: Image9,
+    color: "#CDF9E1",
+  },
+  {
+    name: "모든 일이 잘되는 부적",
+    image: Image10,
+    color: "#EAE3DF",
+  },
+  {
+    name: "운빨 상승 부적",
+    image: Image11,
+    color: "#faeca4",
+  },
+  {
+    name: "살이 빠지는 부적",
+    image: Image12,
+    color: "#E5E5E5",
+  },
+  {
+    name: "걱정이 사라지는 부적",
+    image: Image13,
+    color: "#F8C89D",
+  },
+  {
+    name: "쾌변 부적",
+    image: Image14,
+    color: "#CCBAAD",
+  },
+  {
+    name: "속상한 일이 사라지는 부적",
+    image: Image15,
+    color: "#CCDDE5",
+  },
+  {
+    name: "꿀잠 자는 부적",
+    image: Image16,
+    color: "#ECE2F2",
+  },
+  {
+    name: "건강해지는 부적",
+    image: Image17,
+    color: "#d0f4ce",
+  },
+  {
+    name: "취뽀 부적",
+    image: Image18,
+    color: "#FFDEF8",
+  },
+  {
+    name: "용기가 생기는 부적",
+    image: Image19,
+    color: "#F99DA1",
   },
 ];
